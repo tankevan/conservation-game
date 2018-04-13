d775c01fb70c819b85cfdf9ed5c22d09be964e59
Kevan Tan <kevan.tan@live.com>
2018-04-13 11:50:13 +0800

    added helper mechanic

--- js/game.js
+++ js/game.js
@@ -10,18 +10,11 @@ let gameScene = new Phaser.Scene('Game');
 
 // some parameters for our scene
 gameScene.init = function() {
-  this.canMove = true;
   this.playerSpeed = 2.5;
-  this.targetX = null;
-  this.targetY = null;
-  this.targetName = null;
-  this.targetObj = null;
   this.startingHealth = 500;
   this.globalHealth = this.startingHealth;
-
-  this.onHand = null;
-  this.gameActions = []; // contains arrays of [x, y, name, obj]
   this.updateNumbers = false;
+  this.shouldAddHelper = true;
 }
 
 // load asset files for our game
@@ -53,6 +46,15 @@ gameScene.create = function() {
 
   // scale down
   this.player.setScale(0.5);
+
+  // player properties
+  this.player.canMove = true;
+  this.player.targetX = null;
+  this.player.targetY = null;
+  this.player.targetName = null;
+  this.player.targetObj = null;
+  this.player.onHand = null;
+  this.player.gameActions = []; // contains arrays of [x, y, name, obj]
 
   // add dragon trash bin
   this.recyclingBin = this.add.sprite(30, 80, 'dragon');
@@ -96,8 +98,11 @@ gameScene.create = function() {
     loop: true,
   });
 
-  // creates empty game action order group
+  // creates empty game action order group for the action labels
   this.orderGroup = this.add.group();
+
+  // creates empty helper group
+  this.helperGroup = this.add.group();
 };
 
 // *** CREATE SUB-FUNCTIONS *** 
@@ -121,6 +126,7 @@ function addTrash() {
 
     trash.name = "trash";
     trash.clicked = false;
+    trash.isTargeted = false;
     trash.on('pointerup', function (pointer) {
       const name = this.name; // pass name of object for identification
       gameScene.clickedSprite(pointer, name, this);
@@ -143,6 +149,7 @@ function addAnimal() {
     
     animal.name = "animal";
     animal.clicked = false;
+    animal.isTargeted = false;
     animal.on('pointerup', function (pointer) {
       gameScene.clickedSprite(pointer, this.name, this);
     })
@@ -153,29 +160,32 @@ function addAnimal() {
 
 gameScene.clickedSprite = function(pointer, name, objRef) {
   if (!objRef.clicked && name !== 'recyclingBin') {
-    objRef.clicked = true;
-    this.gameActions.push([objRef.x, objRef.y, name, objRef]);
+    if (!objRef.isTargeted) {
+      objRef.clicked = true;
+      objRef.isTargeted = true;
+      this.player.gameActions.push([objRef.x, objRef.y, name, objRef]);
+    }
   } else if (objRef.clicked && name !== 'recyclingBin') {
     objRef.clicked = false;
+    objRef.isTargeted = false;
     this.removeAction(objRef.x, objRef.y);
   } else {
-    this.gameActions.push([objRef.x, objRef.y, name, objRef]);
+    this.player.gameActions.push([objRef.x, objRef.y, name, objRef]); 
   }
   this.updateNumbers = true;
 }
 
 gameScene.removeAction = function(xPos, yPos) {
-  this.gameActions.forEach((value, index) => {
+  this.player.gameActions.forEach((value, index) => {
     if (value[0] === xPos && value[1] === yPos) {
-      console.log("deleting action");
-      if (this.gameActions.length >= index + 2 && this.gameActions[index+1][2] == 'recyclingBin') {
-        this.gameActions.splice(index, 2);
+      if ( this.player.gameActions.length >= index + 2 &&  this.player.gameActions[index+1][2] == 'recyclingBin') {
+        this.player.gameActions.splice(index, 2);
       } else {
-        this.gameActions.splice(index, 1);
+        this.player.gameActions.splice(index, 1);
       }
-      this.targetX = null;
-      this.targetY = null;
-      this.targetName = null;
+      this.player.targetX = null;
+      this.player.targetY = null;
+      this.player.targetName = null;
     }
   })
 }
@@ -193,58 +203,145 @@ gameScene.update = function() {
   }
 
   // resolve previous action since coord is reached
-  if (!this.targetX && !this.targetY && this.targetName) {
-    if (this.targetName === "trash") {
-      if (!this.onHand) { this.reachTrash(this.targetObj) }
-    } else if (this.targetName === "recyclingBin") {
-      if (this.onHand) { this.reachRecycling() }
-    } else if (this.targetName === 'animal') {
-      if (!this.onHand) {
-        this.canMove = false;
-        console.log(this.canMove);
-        const currObj = gameScene.targetObj;
+  if (! this.player.targetX && ! this.player.targetY &&  this.player.targetName) {
+    if ( this.player.targetName === "trash") {
+      if (! this.player.onHand) { this.reachTrash(this.player,  this.player.targetObj) }
+    } else if ( this.player.targetName === "recyclingBin") {
+      if ( this.player.onHand) { this.reachRecycling(this.player) }
+    } else if ( this.player.targetName === 'animal') {
+      if (! this.player.onHand) {
+        this.player.canMove = false;
+        const currObj = gameScene.player.targetObj;
         this.time.delayedCall(2000, function() {
-          gameScene.reachAnimal(currObj);
+          gameScene.reachAnimal(this.player, currObj);
         }, [], this);
       }
     }
   
-    this.targetName = null;
-    this.gameActions.shift();
+    this.player.targetName = null;
+    this.player.gameActions.shift();
     this.updateNumbers = true;
   }
   
 
   // updates with next action if no curr action
-  if (this.canMove && !this.targetX && !this.targetY && this.gameActions.length > 0) {
+  if ( this.player.canMove && ! this.player.targetX && ! this.player.targetY &&  this.player.gameActions.length > 0) {
     // sets next coordinate as target
-    const newCoord = this.gameActions[0];
-    this.targetX = newCoord[0];
-    this.targetY = newCoord[1];
-    this.targetName = newCoord[2];
-    this.targetObj = newCoord[3];
+    const newCoord =  this.player.gameActions[0];
+    this.player.targetX = newCoord[0];
+    this.player.targetY = newCoord[1];
+    this.player.targetName = newCoord[2];
+    this.player.targetObj = newCoord[3];
   }
 
   // move player when new position clicked
-  if (this.canMove && this.targetX && Math.abs(this.player.x - this.targetX) > 2) {
-    if (this.player.x > this.targetX) {
-      this.movePlayer(-this.playerSpeed, 0); // move x by playerSpeed and y by 0
+  if ( this.player.canMove &&  this.player.targetX && Math.abs( this.player.x -  this.player.targetX) > 2) {
+    if (this.player.x >  this.player.targetX) {
+      this.movePlayer(this.player, -this.playerSpeed, 0); // move x by playerSpeed and y by 0
     } else {
-      this.movePlayer(this.playerSpeed, 0);
+      this.movePlayer(this.player, this.playerSpeed, 0);
     }
   } else {
-    this.targetX = null; // set target to null when x-coord reached
+    this.player.targetX = null; // set target to null when x-coord reached
   }
 
-  if (this.targetY && Math.abs(this.player.y - this.targetY) > 2) {
-    if (this.player.y > this.targetY) {
-      this.movePlayer(0, -this.playerSpeed);
+  if ( this.player.targetY && Math.abs(this.player.y -  this.player.targetY) > 2) {
+    if (this.player.y >  this.player.targetY) {
+      this.movePlayer(this.player, 0, -this.playerSpeed);
     } else {
-      this.movePlayer(0, this.playerSpeed);
+      this.movePlayer(this.player, 0, this.playerSpeed);
     }
   } else {
-    this.targetY = null;
+    this.player.targetY = null;
   }
+
+  // !! helper behavior
+  this.helperGroup.children.each((helper) => {
+    // if at item, perform action
+    if (! helper.targetX && ! helper.targetY &&  helper.targetName) {
+      if ( helper.targetName === "trash") {
+        if (! helper.onHand) { this.reachTrash(helper,  helper.targetObj) }
+      } else if ( helper.targetName === "recyclingBin") {
+        if ( helper.onHand) { this.reachRecycling(helper) }
+      } else if ( helper.targetName === 'animal') {
+        if (!helper.onHand) {
+          helper.canMove = false;
+          console.log(helper.canMove);
+          const currObj = helper.targetObj;
+          this.time.delayedCall(2000, function() {
+            gameScene.reachAnimal(helper, currObj);
+          }, [], this);
+        }
+      }
+    
+      helper.targetName = null;
+      helper.gameActions.shift();
+      this.updateNumbers = true;
+    }
+
+    // if no targetX and targetY, get new action
+    if (!helper.targetX && !helper.targetY) {
+      if (helper.onHand) {
+        const target = gameScene.recyclingBin;
+        helper.targetX = target.x;
+        helper.targetY = target.y;
+        helper.targetName = "recyclingBin";
+      } else {
+        if (helper.collectType === 'trash') {
+          const arrSize = gameScene.trashGroup.children.size;
+          if (arrSize > 0) {
+            for (var i = 0; i < arrSize; i++) {
+              const target = gameScene.trashGroup.children.entries[i];
+              if (!target.isTargeted) {
+                helper.targetX = target.x;
+                helper.targetY = target.y;
+                helper.targetObj = target;
+                helper.targetName = "trash";
+                gameScene.trashGroup.children.entries[i].isTargeted = true;
+                break;
+              }
+            };
+          }
+        } else if (helper.collectType === 'animal') {
+          const arrSize = gameScene.animalGroup.children.size;
+          if (arrSize > 0) {
+            for (var i = 0; i < arrSize; i++) {
+              const target = gameScene.animalGroup.children.entries[i];
+              if (!target.isTargeted) {
+                helper.targetX = target.x;
+                helper.targetY = target.y;
+                helper.targetName = "animal";
+                helper.targetObj = target;
+                gameScene.animalGroup.children.entries[i].isTargeted = true;
+                break;
+              }
+            };
+          }
+        }
+      }
+    }
+
+    // if not at targetX and targetY, move there
+    if (helper.canMove && helper.targetX && Math.abs(helper.x - helper.targetX) > 2) {
+      if (helper.x > helper.targetX) {
+        this.movePlayer(helper, -helper.playerSpeed, 0); // move x by playerSpeed and y by 0
+      } else {
+        this.movePlayer(helper, helper.playerSpeed, 0);
+      }
+    } else {
+      helper.targetX = null; // set target to null when x-coord reached
+    }
+  
+    if (helper.canMove && helper.targetY && Math.abs(helper.y - helper.targetY) > 2) {
+      if (helper.y > helper.targetY) {
+        this.movePlayer(helper, 0, -helper.playerSpeed);
+      } else {
+        this.movePlayer(helper, 0, this.playerSpeed);
+      }
+    } else {
+      helper.targetY = null;
+    }
+  });
 
   // update text of ordering of actions
   if (this.updateNumbers) {
@@ -252,44 +349,75 @@ gameScene.update = function() {
       label.destroy();
     })
     this.orderGroup.children.clear();
-    this.gameActions.forEach((value, index) => {
+    this.player.gameActions.forEach((value, index) => {
       const txt = this.add.text(value[0], value[1], index + 1)
       this.orderGroup.add(txt);
     })
     this.updateNumbers = false;
   }
+
+ //if (this.shouldAddHelper) {
+  if (this.helperGroup.children.size < 2) {
+    this.addHelper();
+  }
 };
 
 // *** UPDATE SUB-FUNCTIONS *** 
 
-gameScene.movePlayer = function(xMove, yMove) {
-  this.player.x += xMove;
-  this.player.y += yMove;
-  if (this.onHand) {
-    this.onHand.x += xMove;
-    this.onHand.y += yMove;
+gameScene.movePlayer = function(player, xMove, yMove) {
+  player.x += xMove;
+  player.y += yMove;
+  if (player.onHand) {
+    player.onHand.x += xMove;
+    player.onHand.y += yMove;
   }
 }
 
-gameScene.reachTrash = function(trash) {
+gameScene.reachTrash = function(player, trash) {
   this.trashGroup.remove(trash);
-  this.onHand = trash;
-  trash.x = this.player.x;
-  trash.y = this.player.y;
+  player.onHand = trash;
+  trash.x = player.x;
+  trash.y = player.y;
 }
 
-gameScene.reachAnimal = function(animal) {
+gameScene.reachAnimal = function(player, animal) {
   this.animalGroup.remove(animal);
   animal.destroy();
-  this.canMove = true;
-  console.log(this.canMove);
-}
-
-gameScene.reachRecycling = function() {
-  this.onHand.destroy();
+  player.canMove = true;
   this.globalHealth += 10;
   globalHealthText.setText(this.globalHealth);
-  this.onHand = null;
+}
+
+gameScene.reachRecycling = function(player) {
+  player.onHand.destroy();
+  this.globalHealth += 10;
+  globalHealthText.setText(this.globalHealth);
+  player.onHand = null;
+}
+
+gameScene.addHelper = function() {
+  const helperX = this.sys.game.config.width / 2;
+  const helperY = this.sys.game.config.height / 2;
+  const helper = this.add.sprite(helperX, helperY, 'player')
+                         .setScale(0.5);
+  
+  helper.playerSpeed = 2.5;
+  helper.canMove = true;
+  helper.targetX = 200;
+  helper.targetY = 200;
+  helper.targetName = null;
+  helper.targetObj = null;
+  helper.onHand = null;
+  helper.gameActions = []; // contains arrays of [x, y, name, obj]
+  if (Math.random() > 0.5) {
+    helper.collectType = 'trash';
+  } else {
+    helper.collectType = 'animal';
+  }
+  console.log(helper.collectType);
+
+  this.helperGroup.add(helper);
+  this.shouldAddHelper = false;
 }
 
 // *************************
